b5e4e9f3c5ff5313464f7ee2ab6c43248941c4da
miatoon <thoudayer@gmail.com>
2025-01-21 21:53:48 -0500

    Android and Perforce

---
+++ .profile.d/android.sh
@@ -0,0 +1,7 @@
+alias scrcpy='scrcpy -m 1000 --no-audio'
+alias scrcpy_slowdevice='scrcpy -m 1000 --no-audio --display-buffer=1'
+alias scrcpy_sd=scrcpy_slowdevice
+alias scrcpy_nocontrol='scrcpy -m 1000 --no-audio --no-control'
+alias scrcpy_cn=scrcpy_nocontrol
+alias scrcpy_slowdevice_nocontrol='scrcpy -m 1000 --no-audio --no-control --display-buffer=1'
+alias scrcpy_sd_nc=scrcpy_slowdevice_nocontrol
---
+++ .profile.d/perforce.sh
@@ -0,0 +1,4 @@
+alias p4-list-changelists-since="p4 changes -e"
+alias p4-list-conflicts="p4 resolve -N"
+alias p4-last-changelist="p4 changes -m1
+"
