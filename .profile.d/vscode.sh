b60a26dd9c0224ba219be37f7cce51d59d1ed638
miatoon <thoudayer@gmail.com>
2025-03-24 18:33:08 -0400

    Handle badly installed VSCode when it is not in the `PATH`

--- .profile.d/vscode.sh
+++ .profile.d/vscode.sh
@@ -1,4 +1,13 @@
 which code >& /dev/null
+if [ $? -ne 0 ]; then
+    if [ -f "${HOME}/AppData/Local/Programs/Microsoft VS Code/Code.exe" ]; then
+        alias code='"${HOME}/AppData/Local/Programs/Microsoft VS Code/Code.exe"'
+    elif [ -f "/c/Program Files/Microsoft\ VS\ Code/bin/code.exe" ]; then
+        alias code='"/c/Program Files/Microsoft\ VS\ Code/bin/code"'
+    fi
+fi
+
+which code >& /dev/null
 if [ $? -eq 0 ]; then
     alias cn='code -n'
 fi
