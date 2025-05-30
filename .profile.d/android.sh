06ece620b4b690b583f882f52a76fd2610cd5a7b
miatoon <thoudayer@gmail.com>
2025-05-30 10:58:50 -0400

    Defined aliases only if `scrcpy` is present

--- .profile.d/android.sh
+++ .profile.d/android.sh
@@ -1,7 +1,10 @@
-alias scrcpy='scrcpy -m 1000 --no-audio'
-alias scrcpy_slowdevice='scrcpy -m 1000 --no-audio --display-buffer=1'
-alias scrcpy_sd=scrcpy_slowdevice
-alias scrcpy_nocontrol='scrcpy -m 1000 --no-audio --no-control'
-alias scrcpy_cn=scrcpy_nocontrol
-alias scrcpy_slowdevice_nocontrol='scrcpy -m 1000 --no-audio --no-control --display-buffer=1'
-alias scrcpy_sd_nc=scrcpy_slowdevice_nocontrol
+which scrcpy >& /dev/null
+if [ $? -eq 0 ]; then
+    alias scrcpy='scrcpy -m 1000 --no-audio'
+    alias scrcpy_slowdevice='scrcpy -m 1000 --no-audio --display-buffer=1'
+    alias scrcpy_sd=scrcpy_slowdevice
+    alias scrcpy_nocontrol='scrcpy -m 1000 --no-audio --no-control'
+    alias scrcpy_cn=scrcpy_nocontrol
+    alias scrcpy_slowdevice_nocontrol='scrcpy -m 1000 --no-audio --no-control --display-buffer=1'
+    alias scrcpy_sd_nc=scrcpy_slowdevice_nocontrol
+fi
