b62fbff55716838e74fbb0d4b395b6b91dd8b7a0
miatoon <thoudayer@gmail.com>
2025-05-30 10:42:36 -0400

    Define a git alias only if git is present

--- .profile.d/git-bash.sh
+++ .profile.d/git-bash.sh
@@ -1,8 +1,10 @@
-function explore-with-git-bash() {
-    (
-        cd "$1" && nohup.exe "/c/Program Files/Git/git-bash.exe" &> /dev/null
-    ) &
-}
+if [ -f "/c/Program Files/Git/git-bash.exe" ]; then
+    function explore-with-git-bash() {
+        (
+            cd "$1" && nohup.exe "/c/Program Files/Git/git-bash.exe" &> /dev/null
+        ) &
+    }
+fi
 
 function wpwd() {
     cygpath --windows "$(pwd)"
