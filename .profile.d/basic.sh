ab037fc76c4a189ccd88dd7b83c52bc72b09968c
miatoon <thoudayer@gmail.com>
2025-05-30 10:57:01 -0400

    Add comment on why `--file-type` is used instead of `-F`

--- .profile.d/basic.sh
+++ .profile.d/basic.sh
@@ -1,3 +1,5 @@
+# The "ls -F" option is more eager then the "ls --file-type" option, as the "-F"
+# use an open-file event which can trigger any VFS system.
 alias ls='ls --file-type --color=never' # Ending-character file type
 alias la='ls -A'                        # all but . and ..
 
