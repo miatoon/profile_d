94d2ce04383bd9c9d38a01d0bf2f20ebee0467e9
miatoon <thoudayer@gmail.com>
2025-05-31 12:26:19 -0400

    Add function to launch git-bash without Windows Terminal

--- .profile.d/explore.sh
+++ .profile.d/explore.sh
@@ -18,4 +18,13 @@ if [[ "${OS}" =~ "Windows" ]] ; then
             ) &
         }
     fi
+
+
+    if [ -f "/c/Program Files/Git/git-bash.exe" ]; then	
+        function explore-with-git-bash() {
+            (
+                cd "$1" && nohup.exe "/c/Program Files/Git/git-bash.exe" &> /dev/null
+            ) &
+        }
+    fi
 fi
