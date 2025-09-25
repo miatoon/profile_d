bdb7b3266102e3f906395513d13d6bf41f6034ec
miatoon <thoudayer@gmail.com>
2025-09-25 08:01:00 -0400

    Redefined some time aliases

--- .profile.d/time.sh
+++ .profile.d/time.sh
@@ -1,1 +1,3 @@
-alias timestamp="date '+%Y-%m-%d_%H-%M-%S'"
+alias timestampc="date '+%Y%m%d%H%M%S'"  # Compact
+alias timestamp="date '+%Y%m%d_%H%M%S'"  # Default
+alias timestamph="date '+%Y-%m-%d_%H-%M-%S'"  # Human
