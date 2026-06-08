af2d30024f75bf99513baa50c245c5f7142a2c62
miatoon <80300952+miatoon@users.noreply.github.com>
2026-06-08 09:21:42 -0400

    Update time.sh, add `timestamp_to_date()`

--- .profile.d/time.sh
+++ .profile.d/time.sh
@@ -1,3 +1,20 @@
 alias timestampc="date '+%Y%m%d%H%M%S'"  # Compact
 alias timestamp="date '+%Y%m%d_%H%M%S'"  # Default
 alias timestamph="date '+%Y-%m-%d_%H-%M-%S'"  # Human
+
+function timestamp_to_date() {
+    local timestamp=$1
+    local milli=""
+    if [ ${#timestamp} -gt 14 ]; then
+        echo "bad timestamp"
+        return
+    fi
+    if [ ${#timestamp} -gt 10 ]; then
+        milli=${timestamp:10}
+        timestamp=${timestamp:0:10}
+        res=$(date -d @${timestamp} +'%Y-%m-%d %H:%M:%S')
+        echo "${res}.${milli}"
+    else
+        date -d @${timestamp} +'%Y-%m-%d %H:%M:%S'
+    fi
+}
